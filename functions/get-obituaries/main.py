07bf63a32224dc567c10f4e609a5327913367e13
Will <williamgfraser2015@gmail.com>
2023-04-10 17:54:54 -0600

    appended ids

--- functions/get-obituaries/main.py
+++ functions/get-obituaries/main.py
@@ -12,7 +12,7 @@ def get_obituaries_handler(event, context):
         CLOUDINARY_API_KEY = "942673461688855"
         CLOUDINARY_NAME = "duoghyw7n"
         
-        dynamodb = boto3.theresource("dynamodb")
+        dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("thelastshow-30158991")
 
         response = table.scan()
@@ -23,23 +23,27 @@ def get_obituaries_handler(event, context):
                 ExclusiveStartKey=response['LastEvaluatedKey'])
             data.extend(response['Items'])
 
-        # ___________________________________________________________________
-        # TODO WILL CAN YOU POPULATE THESE LISTS WITH THE IDS FROM THE DYNAMODB
+
         img_ids = []
         audio_ids = []
+
+        for item in data:
+            img_ids.append(item['img_id'])
+            audio_ids.append(item['audio_id'])
+
         imgs = []
         audio_files = []
 
         # Cloudinary API Request to get all the images
         for id in img_ids:
             img_response = requests.get(
-                f"https://res.cloudinary.com/{CLOUDNAME}/image/upload/{id}")
+                f"https://res.cloudinary.com/{CLOUDINARY_NAME}/image/upload/{id}")
             imgs.append(img_response.content)
 
         # Cloudnary API Request to get all the audio files
         for id in audio_ids:
             audio_response = requests.get(
-                f"https://res.cloudinary.com/{CLOUDNAME}/video/upload/{id}")
+                f"https://res.cloudinary.com/{CLOUDINARY_NAME}/video/upload/{id}")
             audio_files.append(audio_response.content)
 
         # ___________________________________________________________________
