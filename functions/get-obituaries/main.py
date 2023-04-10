11832ade70aed6bbbfe40e132446efaf113d8d99
Will <williamgfraser2015@gmail.com>
2023-04-10 16:02:32 -0600

    finsihed get handler

--- functions/get-obituaries/main.py
+++ functions/get-obituaries/main.py
@@ -1,4 +1,27 @@
 # add your get-obituaries function here
+import boto3
+import json 
 
 def get_obituaries_handler(event, context):
-    pass
+    try:
+
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table("thelastshow-30158991")
+
+        response = table.scan()
+        data = response['Items']
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            data.extend(response['Items'])
+        
+        return {
+            'statusCode': 201,
+            'body': json.dumps(data),
+        }
+        
+    except Exception as e:
+        return {
+            'statusCode': 401,
+            'body': json.dumps(f'Error retrieving obituaries: {str(e)}'),
+        }
