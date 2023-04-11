4f76baf13fd3244a11622883f447cabfbd69a00b
Will <williamgfraser2015@gmail.com>
2023-04-11 16:24:24 -0600

    removed commented out preset cards

--- src/components/Table.js
+++ src/components/Table.js
@@ -20,28 +20,6 @@ function Table({ obituaries }) {
             />
           );
         })}
-        {/* <AbituaryCard
-          className="card"
-          // imageSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg/800px-Steve_Jobs_Headshot_2010-CROP_%28cropped_2%29.jpg"
-          imageId="ci2bgkm0om5ox99kekl7"
-          audioId="lbel0m2ouusgqtqya4l5"
-          name="Aarsh"
-          born="November 9, 2003"
-          died="Present"
-          bio="Lorem ipsum dolor sit amet. Et galisum provident et voluptas nisi aut perferendis reprehenderit. 
-Ut delectus exercitationem ex reiciendis sapiente aut temporibus inventore."
-        />
-        <AbituaryCard
-          className="card"
-          imageId="ci2bgkm0om5ox99kekl7"
-          audioId="lbel0m2ouusgqtqya4l5"
-          name="Aarsh"
-          born="November 9, 2003"
-          died="Present"
-          bio="Lorem ipsum dolor sit amet. Et galisum provident et voluptas nisi aut perferendis reprehenderit. 
-Ut delectus exercitationem ex reiciendis sapiente aut temporibus inventore. 
-Et perferendis assumenda ex reprehenderit rerum id voluptatem quisquam."
-        /> */}
       </div>
     </div>
   );
