abc1b00f4068aee604a29a4aad6edccf9920fb7f
AarshShah9 <aarshshah99@gmail.com>
2023-04-10 20:19:54 -0600

    adding css changes to the cards

--- src/components/AbituaryCard.js
+++ src/components/AbituaryCard.js
@@ -4,6 +4,17 @@ import "./AbituaryCard.css";
 
 function AbituaryCard({ imageSrc, audioSrc, name, born, died, bio }) {
   const [cardOpen, setCardOpen] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  let audio = new Audio(audioSrc);
+
+  const handlePlay = () => {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
 
   const toggleCard = () => {
     setCardOpen(!cardOpen);
@@ -17,12 +28,18 @@ function AbituaryCard({ imageSrc, audioSrc, name, born, died, bio }) {
         <p className="date">
           {born} - {died}
         </p>
+
         {/* update below to have transition */}
         <div className={cardOpen ? "card-open" : "card-closed"}>
           <p className="bio">{bio}</p>
-          <audio controls>
+          {/* <audio controls>
             <source src={audioSrc} type="audio/mpeg" />
-          </audio>
+          </audio> */}
+          <div className="audio-button-wrapper">
+            <button onClick={handlePlay} className="audio-button">
+              <img />
+            </button>
+          </div>
         </div>
       </div>
     </div>
