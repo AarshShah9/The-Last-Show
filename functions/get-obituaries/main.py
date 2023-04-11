0793301c325081935bb4a440271ee94e87647d4f
Will <williamgfraser2015@gmail.com>
2023-04-11 09:58:00 -0600

    removed cloudinary, changed return

--- functions/get-obituaries/main.py
+++ functions/get-obituaries/main.py
@@ -6,12 +6,7 @@ import requests
 
 
 def get_obituaries_handler(event, context):
-    try:
-        CLOUDINARY_SECRET = boto3.resource('ssm').get_parameter(
-            Name='/thelastshow/cloudinary_secret', WithDecryption=True)['Parameter']['Value']
-        CLOUDINARY_API_KEY = "942673461688855"
-        CLOUDINARY_NAME = "duoghyw7n"
-        
+    try:        
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("thelastshow-30158991")
 
@@ -24,33 +19,33 @@ def get_obituaries_handler(event, context):
             data.extend(response['Items'])
 
 
-        img_ids = []
-        audio_ids = []
+        # img_ids = []
+        # audio_ids = []
 
-        for item in data:
-            img_ids.append(item['img_id'])
-            audio_ids.append(item['audio_id'])
+        # for item in data:
+        #     img_ids.append(item['image'])
+        #     audio_ids.append(item['audio'])
 
-        imgs = []
-        audio_files = []
+        # imgs = []
+        # audio_files = []
 
-        # Cloudinary API Request to get all the images
-        for id in img_ids:
-            img_response = requests.get(
-                f"https://res.cloudinary.com/{CLOUDINARY_NAME}/image/upload/{id}")
-            imgs.append(img_response.content)
+        # # Cloudinary API Request to get all the images
+        # for id in img_ids:
+        #     img_response = requests.get(
+        #         f"https://res.cloudinary.com/{CLOUDINARY_NAME}/image/upload/{id}")
+        #     imgs.append(img_response.content)
 
-        # Cloudnary API Request to get all the audio files
-        for id in audio_ids:
-            audio_response = requests.get(
-                f"https://res.cloudinary.com/{CLOUDINARY_NAME}/video/upload/{id}")
-            audio_files.append(audio_response.content)
+        # # Cloudnary API Request to get all the audio files
+        # for id in audio_ids:
+        #     audio_response = requests.get(
+        #         f"https://res.cloudinary.com/{CLOUDINARY_NAME}/video/upload/{id}")
+        #     audio_files.append(audio_response.content)
 
         # ___________________________________________________________________
 
         return {
             'statusCode': 201,
-            'body': json.dumps(data),
+            'body': data
         }
 
     except Exception as e:
